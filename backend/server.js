26300e4d6b81b5aa9661b0b4f18c1a7c65645bfc
anaskhan 88 <pathananas2006@gmail.com>
2025-08-28 20:07:02 +0530

    Add import

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,12 @@
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-const express = require("express");
-const mongoose =  require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
